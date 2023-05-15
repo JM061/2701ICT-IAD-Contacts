163f3ba4116eb813132dd25b231a1bfdcadb5c91
JM061 <jrmurph06@gmail.com>
2023-05-16 06:52:59 +1000

    added stuff for map and storage

--- src/app/account/account.page.ts
+++ src/app/account/account.page.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Style } from '@capacitor/status-bar';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.page.html',
   styleUrls: ['./account.page.scss'],
 })
+
+
 export class AccountPage implements OnInit {
   username: any;
+  map: mapboxgl.Map
+  mapBoxAPIKey:string = 'pk.eyJ1Ijoiam0wNiIsImEiOiJjbGd2cmptNHkwdDNrM2NrbGdmMHV1dTNmIn0.dbloBcA70UMEIoQca9kGsQ';
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.username = this.route.snapshot.paramMap.get('username');
   }
+
+
 }
